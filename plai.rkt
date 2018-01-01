0e8ad16d56fe1e612fcd58c10c40b595679407b5
Tobin Yehle <tobinyehle@gmail.com>
2018-01-01 15:52:19 -0700

    revert all the store passing stuff

--- plai.rkt
+++ plai.rkt
@@ -10,43 +10,21 @@
   [zeroC (n : ExprC)]
   [ifC (c : ExprC) (t : ExprC) (f : ExprC)]
   [appC (fun : ExprC) (arg : ExprC)]
-  [lamC (arg : symbol) (body : ExprC)]
-  [setC (var : symbol) (val : ExprC)]
-  [seqC (b1 : ExprC) (b2 : ExprC)])
+  [lamC (arg : symbol) (body : ExprC)])
 
 (define-type Value
-  [unitV]
   [numV (n : number)]
   [boolV (b : boolean)]
   [closV (arg : symbol) (body : ExprC) (env : Env)])
 
-(define-type-alias Location number)
-
 (define-type Binding
-  [bind (name : symbol) (loc : Location)])
+  [bind (name : symbol) (val : Value)])
 
 (define-type-alias Env (listof Binding))
 (define mt-env empty)
 (define extend-env cons)
 
-(define-type Storage
-  [cell (loc : Location) (val : Value)])
-
-(define-type-alias Store (listof Storage))
-(define mt-store empty)
-(define override-store cons)
-
-(define new-loc
-  (let ([n (box 0)])
-    (lambda ()
-      (begin
-        (set-box! n (+ 1 (unbox n)))
-        (unbox n)))))
-
-(define-type Result
-  [v*s (val : Value) (sto : Store)])
-
-(define (run input) (interp (parse input) mt-env mt-store))
+(define (run input) (interp (parse input) mt-env))
 
 
 ;;;;; Parser ;;;;;
@@ -68,12 +46,6 @@
      (ifC (parse (second (s-exp->list s)))
           (parse (third (s-exp->list s)))
           (parse (fourth (s-exp->list s))))]
-    [(s-exp-match? '{set! SYMBOL ANY} s)
-     (setC (s-exp->symbol (second (s-exp->list s)))
-           (parse (third (s-exp->list s))))]
-    [(s-exp-match? '{begin ANY ANY} s)
-     (seqC (parse (second (s-exp->list s)))
-           (parse (third (s-exp->list s))))]
     [(s-exp-match? '{lambda {SYMBOL} ANY} s)
      (lamC (s-exp->symbol (first (s-exp->list (second (s-exp->list s)))))
            (parse (third (s-exp->list s))))]
@@ -82,70 +54,41 @@
            (parse (second (s-exp->list s))))]
     [(s-exp-match? `SYMBOL s)
      (idC (s-exp->symbol s))]
-    [else (error 'parse (string-append "invalid input" (s-exp->string s)))]))
+    [else (error 'parse "invalid input")]))
 
 
 ;;;;; Interpreter ;;;;;
 
-(define (interp (exp : ExprC) (env : Env) (sto : Store)) : Result
+(define (interp (exp : ExprC) (env : Env)) : Value
   (type-case ExprC exp
-    [boolC (b) (v*s (boolV b) sto)]
-    [numC (n) (v*s (numV n) sto)]
-    [idC (s) (v*s (fetch (lookup s env) sto) sto)]
-    [plusC (l r) (type-case Result (interp l env sto)
-                   [v*s (lv s1) (type-case Result (interp r env s1)
-                                  [v*s (rv s2) (v*s (num-binop + lv rv) s2)])])]
-    [multC (l r) (type-case Result (interp l env sto)
-                   [v*s (lv s1) (type-case Result (interp r env s1)
-                                  [v*s (rv s2) (v*s (num-binop * lv rv) s2)])])]
-    [zeroC (ne) (type-case Result (interp ne env sto)
-                  [v*s (nv s1) (type-case Value nv
-                                 [numV (n) (v*s (boolV (= 0 n)) s1)]
-                                 [else (error 'interp "not a number")])])]
+    [boolC (b) (boolV b)]
+    [numC (n) (numV n)]
+    [idC (s) (lookup s env)]
+    [plusC (l r) (num-binop + (interp l env) (interp r env))]
+    [multC (l r) (num-binop * (interp l env) (interp r env))]
+    [zeroC (ne) (let ([nv (interp ne env)])
+                  (type-case Value nv
+                    [numV (n) (boolV (= 0 n))]
+                    [else (error 'interp "not a number")]))]
     [ifC (c t f)
-         (type-case Result (interp c env sto)
-           [v*s (cv sc)
-                (cond
-                  [(boolV? cv) (if (boolV-b cv) (interp t env sc) (interp f env sc))]
-                  [else (error 'interp "can only test booleans")])])]
+         (let ([ci (interp c env)])
+           (cond
+             [(boolV? ci) (if (boolV-b ci) (interp t env) (interp f env))]
+             [else (error 'interp "can only test booleans")]))]
     [appC (fun arg)
-          (type-case Result (interp fun env sto)
-            [v*s (fun-val fun-sto)
-                 (type-case Value fun-val
-                   [closV (arg-name body clos-env)
-                          (type-case Result (interp arg env fun-sto)
-                            [v*s (arg-val arg-sto)
-                                 (let* ([where (new-loc)]
-                                        [body-env (extend-env
-                                                   (bind arg-name where)
-                                                   clos-env)]
-                                        [body-sto (override-store
-                                                   (cell where arg-val)
-                                                   arg-sto)])
-                                   (interp body body-env body-sto))])]
-                   [else (error 'interp "not a function")])])]
+          (type-case Value (interp fun env)
+            [closV (arg-name body clos-env)
+                   (interp body
+                           (extend-env (bind arg-name (interp arg env)) clos-env))]
+            [else (error 'inter "not a function")])]
     [lamC (arg body)
-          (v*s (closV arg body env) sto)]
-    [setC (var val)
-          (type-case Result (interp val env sto)
-            [v*s (val-val val-sto)
-                 (v*s (unitV) (override-store (cell (lookup var env) val-val) val-sto))])]
-    [seqC (b1 b2)
-          (type-case Result (interp b1 env sto)
-            [v*s (b1-val b1-sto)
-                 (interp b2 env b1-sto)])]))
+          (closV arg body env)]))
 
-(define (lookup (name : symbol) (env : Env)) : Location
+(define (lookup (name : symbol) (env : Env)) : Value
   (cond
     [(empty? env) (error 'lookup "name not found")]
-    [(symbol=? name (bind-name (first env))) (bind-loc (first env))]
+    [(symbol=? name (bind-name (first env))) (bind-val (first env))]
     [else (lookup name (rest env))]))
-
-(define (fetch (loc : Location) (sto : Store)) : Value
-  (cond
-    [(empty? sto) (error 'fetch "location not found")]
-    [(equal? loc (cell-loc (first sto))) (cell-val (first sto))]
-    [else (fetch loc (rest sto))]))
 
 (define (num-binop (op : (number number -> number)) (l : Value) (r : Value)) : Value
   (cond
